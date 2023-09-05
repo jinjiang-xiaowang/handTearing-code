0245e4f336d9e59c11c2e34905ed67819b85260e
xiaowang <505234661@qq.com>
2023-09-05 21:34:06 +0800

    文件分类

---
+++ 2算法/2-1.数组去重.js
@@ -0,0 +1,24 @@
+
+var arr=[1,2,1,2,1,2,3,5]
+// 1.set+扩展运算符
+// console.log([... new Set(arr)])
+
+// 2.filter+indexof
+// var arr1=arr.filter((item,index)=>arr.indexOf(item)==index)
+// console.log(arr1)
+// 3.循环+indexof
+// var arr1=[]
+// for(let i=0;i<arr.length;i++){
+//     if(arr.indexOf(arr[i])==i){
+//         arr1.push(arr[i])
+//     }
+// }
+//  console.log(arr1)
+// 4.includes
+// var arr1=[]
+// for(let i=0;i<arr.length;i++){
+//     if(!arr1.includes(arr[i])){
+//         arr1.push(arr[i])
+//     }
+// }
+// console.log(arr1)
