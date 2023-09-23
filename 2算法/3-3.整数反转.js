934f7c74fff776c1276935688211819aca2652ac
xiaowang <505234661@qq.com>
2023-09-24 00:09:29 +0800

    整数反转

--- 2算法/3-3.整数反转.js
+++ 2算法/3-3.整数反转.js
@@ -1,3 +1,8 @@
-let a=123
-let b=String(a).split('').reverse()
-console.log(b)
+function _reverse(number){
+   if(number>=0){
+    return String(number).split('').reverse().join('')*1
+   }else{
+    return '-'+String(Math.abs(number)).split('').reverse().join('')*1
+   }
+}
+console.log(_reverse(112))
